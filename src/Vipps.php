ca85462f5debc342635b67bd4187c6e404afc777
Fred Carlsen <fred@sjelfull.no>
2019-02-24 13:27:55 +0100

    Some more cleanup

--- src/Vipps.php
+++ src/Vipps.php
@@ -10,6 +10,7 @@
 
 namespace superbig\vipps;
 
+use craft\commerce\Plugin as CommercePlugin;
 use craft\commerce\services\Gateways;
 use craft\commerce\services\OrderAdjustments;
 use craft\commerce\services\OrderHistories;
@@ -55,6 +56,11 @@ class Vipps extends Plugin
      */
     public static $plugin;
 
+    /**
+     * @var bool
+     */
+    public static $commerceInstalled = false;
+
     // Public Properties
     // =========================================================================
 
@@ -80,53 +86,10 @@ class Vipps extends Plugin
             'api'      => Api::class,
         ]);
 
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-            function(RegisterUrlRulesEvent $event) {
-                $event->rules = array_merge($event->rules, [
-                    // @todo handle + callbackPrefix https://github.com/vippsas/vipps-ecom-api/blob/master/vipps-ecom-api.md#express-checkout-payments
-                    // callbackPrefix: https://github.com/vippsas/vipps-ecom-api/blob/master/vipps-ecom-api.md#initiate-payment
-                    'vipps/callbacks/v2/consents/<userId>'                  => 'vipps/callback/consent-removal',
-                    'vipps/callbacks/v2/payments/<orderId>'                 => 'vipps/callback/complete',
-                    'vipps/callbacks/v2/payments/<orderId>/shippingDetails' => 'vipps/callback/shipping-details',
-                    'vipps/express/checkout'                                => 'vipps/express/checkout',
-                ]);
-            }
-        );
+        self::$commerceInstalled = class_exists(CommercePlugin::class);
 
-        Event::on(Gateways::class, Gateways::EVENT_REGISTER_GATEWAY_TYPES, function(RegisterComponentTypesEvent $event) {
-            $event->types[] = Gateway::class;
-        });
-
-        Event::on(OrderHistories::class, OrderHistories::EVENT_ORDER_STATUS_CHANGE, [self::$plugin->getPayments(), 'onStatusChange']);
-        Event::on(OrderAdjustments::class, OrderAdjustments::EVENT_REGISTER_ORDER_ADJUSTERS, [self::$plugin->getExpress(), 'onRegisterOrderAdjusters']);
-
-        Event::on(
-            CraftVariable::class,
-            CraftVariable::EVENT_INIT,
-            function(Event $event) {
-                /** @var CraftVariable $variable */
-                $variable = $event->sender;
-                $variable->set('vipps', VippsVariable::class);
-            }
-        );
-
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function(RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'vipps/default/do-something';
-            }
-        );
-
-        Event::on(
-            Utilities::class,
-            Utilities::EVENT_REGISTER_UTILITY_TYPES,
-            function(RegisterComponentTypesEvent $event) {
-                $event->types[] = VippsUtilityUtility::class;
-            }
-        );
+        // Install event listeners
+        $this->installEventListeners();
 
         Event::on(
             Plugins::class,
@@ -168,6 +131,99 @@ class Vipps extends Plugin
             [
                 'settings' => $this->getSettings(),
             ]
+        );
+    }
+
+    protected function installEventListeners()
+    {
+
+        $this->installGlobalEventListeners();
+
+    }
+
+    public function installGlobalEventListeners()
+    {
+        Event::on(
+            Gateways::class,
+            Gateways::EVENT_REGISTER_GATEWAY_TYPES,
+            function(RegisterComponentTypesEvent $event) {
+                $event->types[] = Gateway::class;
+            }
+        );
+
+        Event::on(
+            OrderHistories::class,
+            OrderHistories::EVENT_ORDER_STATUS_CHANGE,
+            [self::$plugin->getPayments(), 'onStatusChange']
+        );
+
+        Event::on(
+            OrderAdjustments::class,
+            OrderAdjustments::EVENT_REGISTER_ORDER_ADJUSTERS,
+            [self::$plugin->getExpress(), 'onRegisterOrderAdjusters']
+        );
+
+        Event::on(
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function(Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('vipps', VippsVariable::class);
+            }
+        );
+
+        // Handler: Plugins::EVENT_AFTER_LOAD_PLUGINS
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_LOAD_PLUGINS,
+            function() {
+                // Install these only after all other plugins have loaded
+                $request = Craft::$app->getRequest();
+
+                if ($request->getIsSiteRequest() && !$request->getIsConsoleRequest()) {
+                    $this->installSiteEventListeners();
+                }
+
+                if ($request->getIsCpRequest() && !$request->getIsConsoleRequest()) {
+                    $this->installCpEventListeners();
+                }
+            }
+        );
+    }
+
+    protected function installSiteEventListeners()
+    {
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function(RegisterUrlRulesEvent $event) {
+                $event->rules = array_merge($event->rules, [
+                    'vipps/callbacks/v2/consents/<userId>'                  => 'vipps/callback/consent-removal',
+                    'vipps/callbacks/v2/payments/<orderId>'                 => 'vipps/callback/complete',
+                    'vipps/callbacks/v2/payments/<orderId>/shippingDetails' => 'vipps/callback/shipping-details',
+                    'vipps/express/checkout'                                => 'vipps/express/checkout',
+                ]);
+            }
+        );
+    }
+
+    protected function installCpEventListeners()
+    {
+        Event::on(
+            Utilities::class,
+            Utilities::EVENT_REGISTER_UTILITY_TYPES,
+            function(RegisterComponentTypesEvent $event) {
+                $event->types[] = VippsUtilityUtility::class;
+            }
+        );
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function(RegisterUrlRulesEvent $event) {
+                $event->rules['cpActionTrigger1'] = 'vipps/default/do-something';
+            }
         );
     }
 }
